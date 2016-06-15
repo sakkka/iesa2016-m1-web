a7695b890fb919f9102db5f0e44a4d975eb5fcd7
sakkka <mastere1it@FRO-206-20.fro-206.froment.in.iesanetwork.com>
2016-06-15 14:44:59 +0200

    closes #2 ok propre

---
+++ swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Darwin
+
+var str : String = "Hello, playground hahaha ok"
+var index : Double
+
+for index in 1...100 {
+    print(index)
+}
+
+
+var lol : Double = 30
+var lol2 : Double = 10
+
+pow(lol, lol2)
+
+let label = "the width is "
+let width = 94
+let widthLabel = label + String(width)
+
+print("truc de ouf \(widthLabel)")
